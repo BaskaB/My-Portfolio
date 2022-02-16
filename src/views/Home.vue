11b69cdfec9a35889fceca71c876fc4ba40c323a
Baska Babicova <barb.babicova@gmail.com>
2022-02-16 22:07:10 +0100

    About me and Skills

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,8 +25,83 @@
         <a href="#">Projects</a>
       </div>
     </div>
-
   </header>
+  <section class="about-me">
+    <div class="img-wrap">
+      <img src="/images/about-me/girl.png" alt="">
+    </div>
+    <div class="text-wrap">
+      <h1>About me</h1>
+      <h3>Hi, my name is Baška! I am junior frontend developer and also a 3D graphic...</h3>
+      <p>Tiramisu cheesecake bonbon toffee ice cream candy. Candy canes chocolate cake soufflé jujubes biscuit. Ice cream gummies sweet danish brownie liquorice powder bear claw. Muffin gingerbread cheesecake chupa chups soufflé wafer brownie jelly-o. Pie wafer marzipan sweet halvah. Cake soufflé chocolate bar dessert pudding carrot cake.</p>
+    </div>
+  </section>
+  <section class="skills">
+    <div class="bg-wrap">
+      <img src="/images/skills/skills-bg.png" alt="">
+    </div>
+    <div class="skills-content">
+    <div class="title-wrap">
+      <h2>My skills</h2>
+    </div>
+    <div class="flip-cards">
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <div class="text-wrap">
+            <h3>CODING</h3>
+            </div>
+            <div class="img-wrap">
+            <img src="/images/skills/flip-cards/coding-icon.png" alt="">
+            </div>
+          </div>
+          <div class="flip-card-back">
+            <div class="text-wrap">
+            <h3>Frontend development</h3>
+            <p>Icing croissant topping chocolate bar soufflé.Pie topping fruitcake cupcake bonbon. Pudding apple pie pudding chocolate bar donut pastry tiramisu brownie.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+            <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <div class="text-wrap">
+            <h3>GRAPHIC</h3>
+            </div>
+            <div class="img-wrap">
+            <img src="/images/skills/flip-cards/pen.png" alt="">
+            </div>
+          </div>
+          <div class="flip-card-back">
+            <div class="text-wrap">
+            <h3>Graphic designe</h3>
+            <p>Shortbread cookie icing toffee toffee halvah cake topping jelly beans. Jelly-o macaroon carrot cake gummies jelly-o dessert. Pie topping fruitcake cupcake bonbon. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+            <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <div class="text-wrap">
+            <h3>UI</h3>
+            </div>
+            <div class="img-wrap">
+            <img src="/images/skills/flip-cards/UI.png" alt="">
+            </div>
+          </div>
+          <div class="flip-card-back">
+            <div class="text-wrap">
+            <h3>User interface</h3>
+            <p> Pudding apple pie pudding chocolate bar donut pastry tiramisu brownie.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
 </div>
 </template>
 
