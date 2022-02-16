8d28216269c16b95eb233e2652f66b7cdcb9bc86
Baska Babicova <barb.babicova@gmail.com>
2022-02-16 17:23:15 +0100

    Header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="home">
+  <header>
+    <div class="bg-wrap">
+      <img src="/images/header/bg.png" alt="">
+    </div>
+    <div class="header-overlay">
+      <div class="logo-wrap">
+        <img src="/images/header/logo.png" alt="">
+      </div>
+      <div class="social-wrap">
+        <a href="#" class="ig-wrap">
+          <img src="/images/header/instagram.png" alt="">
+        </a>
+        <a href="#" class="in-wrap">
+          <img src="/images/header/linkedin.png" alt="">
+        </a>
+        <a href="#" class="git-wrap">
+          <img src="/images/header/git.png" alt="">
+        </a>
+      </div>
+      <div class="menu-wrap">
+        <a href="#">About me</a>
+        <a href="#">Skills</a>
+        <a href="#">Projects</a>
+      </div>
+    </div>
+
+  </header>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
